2e2f6baa9553dd6a10bc3780a21d7f63eac2d878
drdynscript <drdynscript@gmail.com>
2020-05-05 10:28:32 +0200

    WIP

---
+++ client/src/app/config/index.js
@@ -0,0 +1,4 @@
+export const youtubeDataApi = {
+  apiKey: process.env.REACT_APP_YOUTUBE_DATA_API_KEY,
+  searchUrl: process.env.REACT_APP_YOUTUBE_SEARCH_URL
+};
--- client/src/app/services/youtube.services.js
+++ client/src/app/services/youtube.services.js
@@ -1,6 +1,6 @@
-const YOUTUBE_DATA_API_KEY = 'AIzaSyDLBTKPgGua24GCzqR30U0hwiTYdUjs8Kw';
-const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
-const YOUTUBE_API_SEARCH = `${YOUTUBE_SEARCH_URL}?key=${YOUTUBE_DATA_API_KEY}&part=snippet&maxResults=20&q=`
+import { youtubeDataApi } from '../config'
+
+const YOUTUBE_API_SEARCH = `${youtubeDataApi.searchUrl}?key=${youtubeDataApi.apiKey}&part=snippet&maxResults=20&q=`
 
 class YoutubeApi {
   static getSearchResults = async (q) => {
